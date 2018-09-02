60e1d1a0574080af23d9205c614edb167a0c1a50
Brendan DeMilt <bdemilt98@gmail.com>
2018-09-02 17:36:05 -0400

    Shuffled training data in an achronological order in order to increase model accuracy as it traverses the list linearly

--- TrainingDataFormatter.py
+++ TrainingDataFormatter.py
@@ -4,6 +4,7 @@
 #for the neural net
 #Original Syllable data found at http://delphiforfun.org/programs/syllables.htm
 ###########################
+import random
 
 Input = open("Syllables.txt", "r")
 output = open("Training_Data.txt", "w+")
@@ -16,7 +17,14 @@ longestWord = ''
 #The largest syllable count will be the number of possible outputs
 biggestSyllable = -1
 
-for text in Input:
+
+#In order to increase diversity of data, I'm shuffling the order of the data
+#entries
+
+shuffleList = Input.readlines()
+random.shuffle(shuffleList)
+
+for text in shuffleList:
     arr = text.split("=")
     sylCount = arr[1].count('-')+1
 
@@ -25,12 +33,12 @@ for text in Input:
         
     if len(arr[0]) > len(longestWord):
         longestWord = arr[0]
-
-
+        
     output.write(arr[0] + " " + str(sylCount) + "\n")
-
 
 output.write(str(len(longestWord)) + " " + str(biggestSyllable))
 
-
 output.close()
+
+
+
