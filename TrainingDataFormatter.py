5697f9cf3f49d77a08d734208d2d535520099cc3
Brendan DeMilt <bdemilt98@gmail.com>
2018-09-02 16:20:27 -0400

    Initial Commit

---
+++ TrainingDataFormatter.py
@@ -0,0 +1,5 @@
+###########################
+#Written by Brendan DeMilt
+#Formats pre-Calculated word-syllable combinations into usable training data
+#for the neural net
+###########################
